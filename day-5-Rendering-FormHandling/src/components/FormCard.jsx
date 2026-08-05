1ba72c31808547e680f36ec47f28216b2e56f149
Darshan-Patil08 <darshan.s.patil08@gmail.com>
2026-08-05 23:22:32 +0530

    leaning how to save the user inputs in form

--- day-5-Rendering-FormHandling/src/components/FormCard.jsx
+++ day-5-Rendering-FormHandling/src/components/FormCard.jsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 
 const FormCard = () => {
+
+  const [name, setName] = useState("");
+  console.log(name);
   return (
     <div className="flex p-5 flex-col gap-4">
-        <input className="border text-white border-gray-300 p-2 rounded-md" type="text" placeholder="Enter Your Name" />
+        <input onChange={(e)=>{
+          setName(e.target.value);
+        }} className="border text-white border-gray-300 p-2 rounded-md" type="text" placeholder="Enter Your Name" />
         <input className="border text-white border-gray-300 p-2 rounded-md" type="email" placeholder="Enter Your Email" />
         <input className="border text-white border-gray-300 p-2 rounded-md" type="password" placeholder="Enter Your Password" />
         <button className="bg-blue-500 text-black p-2 rounded-md" type="submit">Submit</button>
