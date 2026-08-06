d8d5a0e2ebc96484680d28780c9620317efd2ad6
Darshan-Patil08 <darshan.s.patil08@gmail.com>
2026-08-06 23:07:49 +0530

    form handling is complete

--- day-5-Rendering-FormHandling/src/components/FormCard.jsx
+++ day-5-Rendering-FormHandling/src/components/FormCard.jsx
@@ -2,16 +2,28 @@ import { useState } from "react";
 
 const FormCard = () => {
 
-  const [name, setName] = useState("");
-  console.log(name);
+const [FormData, setFormData] = useState({});
+
+
+
+let handleChange = (e)=>{
+let {name, value} = e.target;
+  setFormData({
+    ...FormData,
+    [name]: value,
+  })
+};
+  console.log(FormData)
   return (
     <div className="flex p-5 flex-col gap-4">
-        <input onChange={(e)=>{
-          setName(e.target.value);
-        }} className="border text-white border-gray-300 p-2 rounded-md" type="text" placeholder="Enter Your Name" />
-        <input className="border text-white border-gray-300 p-2 rounded-md" type="email" placeholder="Enter Your Email" />
-        <input className="border text-white border-gray-300 p-2 rounded-md" type="password" placeholder="Enter Your Password" />
+        <input name="name" onChange={handleChange} className="border text-white border-gray-300 p-2 rounded-md" type="text" placeholder="Enter Your Name" />
+        <input name="email" onChange={handleChange} className="border text-white border-gray-300 p-2 rounded-md" type="email" placeholder="Enter Your Email" />
+        <input name="password" onChange={handleChange} className="border text-white border-gray-300 p-2 rounded-md" type="password" placeholder="Enter Your Password" />
         <button className="bg-blue-500 text-black p-2 rounded-md" type="submit">Submit</button>
+          
+        <h1 className="text-2xl font-bold text-white">this is the value of name text box - {FormData.name}</h1>
+        <h1 className="text-2xl font-bold text-white">this is the value of email text box - {FormData.email}</h1>
+        <h1 className="text-2xl font-bold text-white">this is the value of password text box - {FormData.password}</h1>
     </div>
 
   )
