5b872d5d214596dcca365ec4b1ad205d6dd13e8c
Darshan-Patil08 <darshan.s.patil08@gmail.com>
2026-08-22 19:35:03 +0530

    Day-11 completed

--- day-11-context-useeffect/src/App.jsx
+++ day-11-context-useeffect/src/App.jsx
@@ -1,25 +1,40 @@
 // import Home from './components/Home'
-// import About from './components/About'
+import About from './components/About'
 import Contact from './components/Contact'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
 const App = () => {
  
   const [count, setCount] = useState(0)
-
+  const [toggle, setToggle] = useState(false)
+  const [products, setProducts] = useState(null)
   useEffect(()=>{
     console.log("App component rendered")
   },[])
 
+  const fetchData = async()=>{
+    const res = await axios.get("https://fakestoreapi.com/products")
+    setProducts(res.data)
+    console.log(res.data)
+  }
 
+  useEffect(()=>{
+    fetchData()
+  },[])
+ 
   return (
 
     <div className="bg-black text-white h-screen flex flex-col items-center justify-center">
       Counter: {count}
       <button onClick={()=>setCount(count+1)} className="bg-blue-500 px-4 py-2 rounded-md mt-4">Increment</button>
       <button onClick={()=>setCount(count-1)} className="bg-red-500 px-4 py-2 rounded-md mt-4">Decrement</button>
-      {/* <Home />
-      <About /> */}
-      <Contact />
+
+     <button onClick={()=>setToggle(prev=>!prev)} className="bg-green-500 px-4 py-2 rounded-md mt-4">Toggle</button>
+
+      {toggle? <About /> : <Contact />}
+
     </div>
   )
 }
--- day-11-context-useeffect/src/components/About.jsx
+++ day-11-context-useeffect/src/components/About.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react'
 
 const About = () => {
-  console.log("About component rendered")
+ 
+
+  let interval = setInterval(()=>{
+    console.log("About component interval")
+  },1000)
+
+  useEffect(()=>{
+    console.log("About component mounted")
+    return () => {
+      clearInterval(interval)
+    }
+  },[])
   return (
     <div>
         <h1>About Us</h1>
